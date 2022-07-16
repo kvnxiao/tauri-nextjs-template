1159307deb3ff55a7e297708b250ac483b3642a3
Kevin Xiao <xlzqwerty1@gmail.com>
2022-07-15 23:56:21 -0700

    Use src folder for Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
 
-import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.scss"
 
 const Home: NextPage = () => {
   return (
