6cbbfd50ddbb9eddb54efa9c045acd2219fad473
Kevin Xiao <xlzqwerty1@gmail.com>
2025-03-01 21:53:41 -0800

    Upgrade to tauri 2.0 and next 15.2

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  distDir: "dist",
+};
+
+export default nextConfig;
