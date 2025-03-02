8782657369e9049891b1455b3822509000626e4d
Kevin Xiao <xlzqwerty1@gmail.com>
2025-03-01 22:20:14 -0800

    Properly upgrade to Tauri 2.3.1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,16 +3,16 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[tauri::command]
 fn greet() -> String {
-    let now = SystemTime::now();
-    let epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
-    format!("Hello world from Rust! Current epoch: {}", epoch_ms)
+  let now = SystemTime::now();
+  let epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
+  format!("Hello world from Rust! Current epoch: {}", epoch_ms)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
